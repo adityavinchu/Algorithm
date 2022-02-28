c7b5e5b7cf90640b730e693df82f0b960605a7d3
aditya <vinchuaditya@gmail.com>
2022-02-28 11:25:14 +0530

    add elements and sort using swap logic

---
+++ Algorithms/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms
+{
+    internal class InsertionSort
+    {
+        public void Sort(string []array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].CompareTo(array[j]) > 0)
+                    {
+                        string temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,41 +10,45 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            BinarySearch binarysearch = new BinarySearch();
-
+            
             Console.WriteLine("Enter List of words saperated by comma',' ");
-
             string input = Console.ReadLine();
             string[] list = input.Split(',');
 
-            List<string> Sortedlist = new List<string>();
+            //BinarySearch binarysearch = new BinarySearch();
+            //List<string> Sortedlist = new List<string>();
+            //foreach (string str in list)
+            //{
+            //    Sortedlist.Add(str);
+            //}
+            //Sortedlist.Sort();
+            //int i = 0;
+            //foreach (string str in Sortedlist)
+            //{
+            //    list[i] = str;
+            //    i++;
+            //}
+            //Console.WriteLine("Enter the string to search");
+            //string tosearch = Console.ReadLine();
+            //int position = binarysearch.Search(list, tosearch, 0, list.Length - 1);
+            //if (position != -1)
+            //{
+            //    Console.WriteLine("Element is present at index: " + position);
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Element is not present");
+            //    Console.ReadLine();
+            //}
 
-            foreach (string str in list)
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.Sort(list);
+            for(int i = 0; i < list.Length; i++)
             {
-                Sortedlist.Add(str);
+                Console.WriteLine(list[i] +" ");
             }
-
-            Sortedlist.Sort();
-            int i = 0;
-            foreach (string str in Sortedlist)
-            {
-                list[i] = str;
-                i++;
-            }
-
-            Console.WriteLine("Enter the string to search");
-            string tosearch = Console.ReadLine();
-            int position = binarysearch.Search(list, tosearch, 0, list.Length - 1);
-            if (position != -1)
-            {
-                Console.WriteLine("Element is present at index: " + position);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Element is not present");
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
     }
 }
