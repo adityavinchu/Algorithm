7a51342ed2c8b3bbb815111b72a21a20122249dc
aditya <vinchuaditya@gmail.com>
2022-02-28 12:11:28 +0530

    prime numbers in range

---
+++ Algorithms/PrimeNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class PrimeNumber
+    {
+        public bool PrimeCheck(int n)
+        {
+            if (n == 0 || n == 1) return false;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        public void PrintPrime(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if (PrimeCheck(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine(); 
+            Console.WriteLine();
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,10 +10,10 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Enter List of words saperated by comma',' ");
-            string input = Console.ReadLine();
-            string[] list = input.Split(',');
+
+            //Console.WriteLine("Enter List of words saperated by comma',' ");
+            //string input = Console.ReadLine();
+            //string[] list = input.Split(',');
 
             //BinarySearch binarysearch = new BinarySearch();
             //List<string> Sortedlist = new List<string>();
@@ -46,13 +46,20 @@ namespace Algorithms
             //insertionSort.Sort(list);
 
 
-            BubbleSort bubbleSort = new BubbleSort();
-            bubbleSort.Sort(list);
-            for (int i = 0; i < list.Length; i++)
-            {
-                Console.WriteLine(list[i] + " ");
-            }
-            Console.ReadLine();
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Sort(list);
+            //for (int i = 0; i < list.Length; i++)
+            //{
+            //    Console.WriteLine(list[i] + " ");
+            //}
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Enter Number upto which you want prime numbers");
+            int num=Convert.ToInt32(Console.ReadLine());
+            PrimeNumber prime = new PrimeNumber();
+            prime.PrintPrime(num);
+
 
         }
     }
